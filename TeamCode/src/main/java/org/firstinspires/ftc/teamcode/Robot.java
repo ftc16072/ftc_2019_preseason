12697e7996e6c037c183c07c06662929888dffdb
Alan Smith <alan@randomsmiths.com>
2019-03-16 20:40:10 -0400

    Files for motor encoding

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -9,6 +9,7 @@ public class Robot {
     private DigitalChannel hallSensor = null;
     private DcMotor testMotor = null;
     private Servo testServo = null;
+    static final int COUNTS_PER_REVOLUTION = 1120; // with 40:1 reduction
 
     void init(OpMode opMode) {
         hallSensor = opMode.hardwareMap.get(DigitalChannel.class, "hall_sensor");
@@ -35,6 +36,21 @@ public class Robot {
     void stop() {
         testMotor.setPower(0.0);
         testServo.setPosition(0.0);
+    }
+
+    int getMotorPosition() {
+        return testMotor.getCurrentPosition();
+    }
+
+    void turnMotorRotations(double rotations, double startSpeed) {
+        int newPosition = (int) rotations * COUNTS_PER_REVOLUTION;
+        testMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        testMotor.setTargetPosition(newPosition);
+        testMotor.setPower(startSpeed);
+    }
+
+    boolean isMotorBusy() {
+        return testMotor.isBusy();
     }
 
 }
