703bc23a9a2aba8b6f3a60685e1b35a6a9fc59fa
Alan Smith <alan@randomsmiths.com>
2019-03-25 20:48:38 -0400

    Add SmallestOpMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SmallestOpMode.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Smallest OpMode", group = "Training")
+// @Disabled
+public class SmallestOpMode extends OpMode {
+    // Code to run ONCE when the driver hits INIT
+    @Override
+    public void init() {
+
+    }
+
+    // Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+    @Override
+    public void loop() {
+
+    }
+}
