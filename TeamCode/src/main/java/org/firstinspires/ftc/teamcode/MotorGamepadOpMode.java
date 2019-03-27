9b76f35501a9a31c1b088460185b2c7f7387a017
Alan Smith <alan@randomsmiths.com>
2019-03-27 19:50:16 -0400

    Adding Gamepad OpModes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorGamepadOpMode.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp()
+// @Disabled
+public class MotorGamepadOpMode extends OpMode {
+    private Robot robot;
+
+    // Code to run ONCE when the driver hits INIT
+    @Override
+    public void init() {
+        robot = new Robot(this);
+    }
+
+    // Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+    @Override
+    public void loop() {
+        if (gamepad1.a) {
+            robot.setSpeed(0.0);
+        } else {
+            robot.setSpeed(0.5);
+        }
+        telemetry.addData("Motor Revs", robot.getRotations());
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorGamepadSpeedOpMode.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp()
+// @Disabled
+public class MotorGamepadSpeedOpMode extends OpMode {
+    private Robot robot;
+
+    // Code to run ONCE when the driver hits INIT
+    @Override
+    public void init() {
+        robot = new Robot(this);
+    }
+
+    // Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+    @Override
+    public void loop() {
+        double speed = gamepad1.left_stick_y;
+        robot.setSpeed(speed);
+        telemetry.addData("Speed", speed);
+        telemetry.addData("Motor Revs", robot.getRotations());
+    }
+}
