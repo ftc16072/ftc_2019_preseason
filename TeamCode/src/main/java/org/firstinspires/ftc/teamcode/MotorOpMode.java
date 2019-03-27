12476c42ec8c06970403d3c35f55c0cfb38d15dc
Alan Smith <alan@randomsmiths.com>
2019-03-27 17:39:27 -0400

    Add MotorOpModes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorOpMode.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp()
+// @Disabled
+public class MotorOpMode extends OpMode {
+    private Robot robot;
+
+    // Code to run ONCE when the driver hits INIT
+    @Override
+    public void init() {
+        robot = new Robot(this);
+    }
+
+    // Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+    @Override
+    public void loop() {
+        robot.setSpeed(0.5);
+        telemetry.addData("Motor Revs", robot.getRotations());
+    }
+}
