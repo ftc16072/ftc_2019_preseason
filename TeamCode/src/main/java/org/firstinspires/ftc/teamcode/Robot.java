da1e3952549844a2a4834f441c1c1df2e1b64abf
Alan Smith <alan@randomsmiths.com>
2019-03-27 19:50:38 -0400

    Adding servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -3,11 +3,13 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class Robot {
     private DigitalChannel hallSensor;
     private DcMotor testMotor;
     private double ticksPerRevTestMotor;
+    private Servo testServo;
 
     Robot(OpMode opMode) {
         hallSensor = opMode.hardwareMap.get(DigitalChannel.class, "hall_sensor");
@@ -16,6 +18,12 @@ public class Robot {
         testMotor = opMode.hardwareMap.get(DcMotor.class, "test_motor");
         testMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         ticksPerRevTestMotor = testMotor.getMotorType().getTicksPerRev();
+
+        testServo = opMode.hardwareMap.get(Servo.class, "test_servo");
+    }
+
+    void setServoPosition(double position) {
+        testServo.setPosition(position);
     }
 
     void setSpeed(double speed) {
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoOpMode.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp()
+// @Disabled
+public class ServoOpMode extends OpMode {
+    private Robot robot;
+
+    // Code to run ONCE when the driver hits INIT
+    @Override
+    public void init() {
+        robot = new Robot(this);
+    }
+
+    // Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+    @Override
+    public void loop() {
+        if (gamepad1.a) {
+            robot.setServoPosition(0.0);
+        } else if (gamepad1.b) {
+            robot.setServoPosition(1.0);
+        } else {
+            robot.setServoPosition(0.5);
+        }
+    }
+}
