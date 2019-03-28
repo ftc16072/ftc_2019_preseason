4ee396effe9bbc2bd4ea711a17508a8b2d72306f
Alan Smith <alan.smith@nutanix.com>
2019-03-28 09:22:43 -0400

    Made it disabled by default

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SmallestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SmallestOpMode.java
@@ -1,10 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp()
-// @Disabled
+@Disabled
 public class SmallestOpMode extends OpMode {
     // Code to run ONCE when the driver hits INIT
     @Override
