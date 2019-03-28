a9d6c33f036fecaa1b2dec7c1a3a27b94f2666b6
Alan Smith <alan.smith@nutanix.com>
2019-03-28 09:21:04 -0400

    removed name and group to make teaching simpler

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SmallestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SmallestOpMode.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp(name = "Smallest OpMode", group = "Training")
+@TeleOp()
 // @Disabled
 public class SmallestOpMode extends OpMode {
     // Code to run ONCE when the driver hits INIT
