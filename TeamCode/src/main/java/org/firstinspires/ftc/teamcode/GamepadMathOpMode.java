6c6e6538378125ff52cfea7f8bf343c3a3c0cb0d
Alan Smith <alan.smith@nutanix.com>
2019-03-29 14:59:26 -0400

    added math opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadMathOpMode.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp()
+// @Disabled
+public class GamepadMathOpMode extends OpMode {
+    private Robot robot = new Robot();
+
+    // Code to run ONCE when the driver hits INIT
+    @Override
+    public void init() {
+        robot.init(hardwareMap);
+    }
+
+
+    private double squareWithSign(double input) {
+        return input * input * Math.signum(input);
+    }
+
+    // Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+    @Override
+    public void loop() {
+        double speed = squareWithSign(gamepad1.left_stick_y);
+
+        robot.setSpeed(speed);
+        telemetry.addData("Speed", speed);
+        telemetry.addData("Motor Revs", robot.getRotations());
+    }
+}
